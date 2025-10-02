9e444a1d1b472541f81b5a951c96e0e6728e9342
Riyat121 <145189781+Riyat121@users.noreply.github.com>
2025-10-02 23:27:09 +0530

    hyye

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 //middleware for handling cors policy 
 app.use(
     cors({
-      origin:'http://localhost:5173',
+      origin:'https://book-store-f-delta.vercel.app/',
       methods:['GET','POST','PUT','DELETE'],
       allowedHeaders:['Content-Type'],  
     })
